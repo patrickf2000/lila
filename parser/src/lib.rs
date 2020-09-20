8a76fab9af32fb1776043669ce702e48884967aa
Patrick Flynn <patrickf4664@protonmail.com>
2020-09-20 15:24:49 -0400

    Added command line arguments for printing AST and LTAC IRs

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -10,19 +10,32 @@ mod lex;
 // Import what we need
 use std::path::Path;
 
+use ast::AstTree;
 use ltac::LtacFile;
+
+// Returns the ast
+pub fn get_ast(path : &String) -> AstTree {
+    let name = get_name(path);
+    let tree = ast_builder::build_ast(path.to_string(), name.clone());
+    tree
+}
 
 // The main parse function
 pub fn parse(path : String) -> LtacFile {
-    let file_path = Path::new(&path);
-    let name = file_path.file_stem()
-        .unwrap().to_os_string()
-        .into_string().unwrap();
-    
-    let tree = ast_builder::build_ast(path, name.clone());
+    let tree = get_ast(&path.to_string());
+    let name = get_name(&path);
     
     let mut ltac_builder = ltac_builder::new_ltac_builder(name.clone());
     let ltac = ltac_builder.build_ltac(&tree);
     
     ltac
+}
+
+// Returns the file name for a given string
+fn get_name(path : &String) -> String {
+    let file_path = Path::new(path);
+    let name = file_path.file_stem()
+        .unwrap().to_os_string()
+        .into_string().unwrap();
+    name
 }
--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,12 @@ use std::env;
 use parser;
 use x86;
 
-mod test1;
-
 fn main() {
     let mut args : Vec<String> = env::args().collect();
     args.remove(0);
     
-    let mut test1 = false;
+    let mut print_ast = false;
+    let mut print_ltac = false;
     let mut input = String::new();
     
     if args.is_empty() {
@@ -19,19 +18,18 @@ fn main() {
     
     for arg in args {
         match arg.as_ref() {
-            "--test1" => test1 = true,
+            "--ast" => print_ast = true,
+            "--ltac" => print_ltac = true,
             _ => input = arg.clone(),
         }
     }
     
-    if test1 {
-        println!("AST:");
-        test1::build_ast();
-        
-        println!("");
-        
-        println!("LTAC:");
-        test1::build_ltac();
+    if print_ast {
+        let ast = parser::get_ast(&input);
+        ast.print();
+    } else if print_ltac {
+        let ltac = parser::parse(input);
+        ltac.print();
     } else {
         let ltac = parser::parse(input);
         x86::compile(&ltac).expect("Codegen failed with unknown error.");
