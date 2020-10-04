6806a8a09a13a7e4337f964f46e25db9bea62e5d
Patrick Flynn <patrickf4664@protonmail.com>
2020-10-04 19:05:21 -0400

    Updated tests

--- test.sh
+++ test.sh
@@ -58,6 +58,7 @@ run_test 'test/cond/*.qk' 'clib' $1
 run_test 'test/func/*.qk' 'clib' $1
 run_test 'test/loop/*.qk' 'clib' $1
 run_test 'test/array/*.qk' 'clib' $1
+run_test 'test/vector/*.qk' 'clib' $1
 run_test 'test/errors/*.qk' 'clib' $1 "error"
 run_test 'test/errors/ltac/*.qk' "clib" $1 "error"
 
