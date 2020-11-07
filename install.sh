7643ed38bcf3014a1f72fef4bad1fd16878b8845
Patrick Flynn <patrickf4664@protonmail.com>
2020-11-06 20:23:27 -0500

    Added Gedit syntax highlighting file

--- install.sh
+++ install.sh
@@ -4,4 +4,7 @@ cargo build --release
 
 sudo cp target/release/dashc /usr/local/bin
 
+sudo cp share/dash.lang /usr/share/gtksourceview-2.0/language-specs/
+sudo cp share/dash.lang /usr/share/gtksourceview-3.0/language-specs/
+
 echo "Done"
