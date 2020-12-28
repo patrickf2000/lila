b8dc34d7ac3f0b67da0508a44b8f77f20441016f
Patrick Flynn <patrickf4664@protonmail.com>
2020-12-28 12:28:21 -0500

    Reverted changes in build-stdlib

--- build-stdlib.sh
+++ build-stdlib.sh
@@ -21,7 +21,6 @@ fi
 # Order matters
 # Build the standard library
 $lilac ../stdlib/x86_64.ls -o x86_64.o --no-link --pic
-$lilac ../stdlib/riscv64.ls -o riscv64.o --no-link --pic
 $lilac ../stdlib/string.ls -o string.o --no-link --pic
 $lilac ../stdlib/io.ls -o io.o --no-link --pic
 $lilac ../stdlib/fs.ls -o fs.o --no-link --pic
@@ -29,7 +28,6 @@ $lilac ../stdlib/text_io.ls -o text_io.o --no-link --pic
 
 $lilac -o liblila.so --lib \
     x86_64.o \
-    riscv64.o \
     string.o \
     io.o \
     fs.o \
@@ -37,15 +35,7 @@ $lilac -o liblila.so --lib \
     
 rm *.o
 
-if [[ $1 == "x86_64" ]] ; then
-    as ../stdlib/x64_start.asm -o lrt.o
-elif [[ $1 == "riscv64" ]] ; then
-    as ../stdlib/riscv64_start.asm -o lrt.o
-else
-    echo "Invalid architecture."
-    echo "Expected x86_64 or riscv64"
-    exit 1
-fi
+as ../stdlib/x64_start.asm -o lrt.o
 
 cd ..
 
