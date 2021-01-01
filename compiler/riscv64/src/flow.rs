e17b2e52b8b43152fd524472e332c3c00518eb79
Patrick Flynn <patrickf4664@protonmail.com>
2021-01-01 15:16:05 -0500

    Bump copyright year

--- compiler/riscv64/src/flow.rs
+++ compiler/riscv64/src/flow.rs
@@ -1,5 +1,5 @@
 // This file is part of the Lila compiler
-// Copyright (C) 2020 Patrick Flynn
+// Copyright (C) 2020-2021 Patrick Flynn
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
