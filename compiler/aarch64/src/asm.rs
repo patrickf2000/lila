771bfe41fa1620a9c20f880cc398be9081cc265a
Patrick Flynn <patrickf4664@protonmail.com>
2021-04-03 11:00:05 -0400

    Added base for new AArch64 code generation with extern/global/label support

---
+++ compiler/aarch64/src/asm.rs
@@ -0,0 +1,71 @@
+//
+// Copyright 2021 Patrick Flynn
+// This file is part of the Ida compiler.
+// Ida is licensed under the BSD-3 license. See the COPYING file for more information.
+//
+
+// This represents AArch64 instructions
+
+#[derive(Clone, PartialEq)]
+#[allow(dead_code)]
+pub enum Arm64Type {
+    Extern,
+    Global,
+    Label
+}
+
+#[derive(Clone, PartialEq)]
+#[allow(dead_code)]
+pub enum Arm64Arg {
+    Empty,
+    
+    Mem(Arm64Reg, i32),
+    
+    Imm32(i32),
+    Imm64(i64),
+    
+    Reg64(Arm64Reg),
+    Reg32(Arm64Reg)
+}
+
+#[derive(Clone, PartialEq)]
+#[allow(dead_code)]
+pub enum Arm64Reg {
+    SP,
+    XZR,
+    
+    X0, X1, X2, X3, X4, X5, X6, X7,
+    X8,
+    X9, X10, X11, X12, X13, X14, X15, X16, X17,
+    X18,
+    X19, X20, X21, X22, X23, X24, X25, X26, X27, X28,
+    X29,
+    X30,
+    
+    W0, W1, W2, W3, W4, W5, W6, W7,
+    W8,
+    W9, W10, W11, W12, W13, W14, W15, W16, W17,
+    W18,
+    W19, W20, W21, W22, W23, W24, W25, W26, W27, W28,
+    W29,
+    W30
+}
+
+#[derive(Clone)]
+pub struct Arm64Instr {
+    pub instr_type : Arm64Type,
+    pub name : String,
+    pub arg1 : Arm64Arg,
+    pub arg2 : Arm64Arg,
+    pub arg3 : Arm64Arg,
+}
+
+pub fn create_arm64_instr(instr_type : Arm64Type) -> Arm64Instr{
+    Arm64Instr {
+        instr_type : instr_type,
+        name : String::new(),
+        arg1 : Arm64Arg::Empty,
+        arg2 : Arm64Arg::Empty,
+        arg3 : Arm64Arg::Empty,
+    }
+}
--- compiler/aarch64/src/lib.rs
+++ compiler/aarch64/src/lib.rs
@@ -9,9 +9,18 @@ use std::io::prelude::*;
 use std::io::BufWriter;
 use std::fs::File;
 
-use parser::ltac::{LtacFile, LtacData, LtacDataType, /*LtacType,*/ LtacInstr/*, LtacArg*/};
+use parser::ltac::{LtacFile, LtacData, LtacDataType, LtacType, LtacInstr/*, LtacArg*/};
+
+mod asm;
+
+use asm::*;
 
 pub fn compile(ltac_file : &LtacFile) -> io::Result<()> {
+    // Translate the code
+    let mut code : Vec<Arm64Instr> = Vec::new();
+    translate_code(&mut code, &ltac_file.code);
+
+    // Create the file
     let mut name = "/tmp/".to_string();
     name.push_str(&ltac_file.name);
     name.push_str(".asm");
@@ -21,7 +30,7 @@ pub fn compile(ltac_file : &LtacFile) -> io::Result<()> {
     let mut writer = BufWriter::new(file);
     
     write_data(&mut writer, &ltac_file.data);
-    write_code(&mut writer, &ltac_file.code);
+    write_code(&mut writer, &mut code);
     
     Ok(())
 }
@@ -62,11 +71,77 @@ fn write_data(writer : &mut BufWriter<File>, data : &Vec<LtacData>) {
         .expect("[AARCH64_data] Write failed in .data");
 }
 
+// Translate the code section
+fn translate_code(code : &mut Vec<Arm64Instr>, input : &Vec<LtacInstr>) {
+    for ln in input {
+        match ln.instr_type {
+            LtacType::Extern => {
+                let mut instr = create_arm64_instr(Arm64Type::Extern);
+                instr.name = ln.name.clone();
+                code.push(instr);
+            },
+            
+            LtacType::Label => {
+                let mut instr = create_arm64_instr(Arm64Type::Label);
+                instr.name = ln.name.clone();
+                code.push(instr);
+            },
+            
+            LtacType::Func => {
+                arm64_build_func(code, &ln);
+            },
+            
+            _ => {},
+        }
+    }
+}
+
 // Write the code section
-fn write_code(writer : &mut BufWriter<File>, _code : &Vec<LtacInstr>) {
+fn write_code(writer : &mut BufWriter<File>, code : &Vec<Arm64Instr>) {
     let line = ".text\n".to_string();
     writer.write(&line.into_bytes())
-        .expect("[_code] Write failed");
+        .expect("[AArch64_code] Write failed");
 
     //let mut stack_size = 0;
+    for ln in code {
+        match ln.instr_type {
+            Arm64Type::Extern => {
+                let mut line = ".extern ".to_string();
+                line.push_str(&ln.name);
+                line.push_str("\n");
+                
+                writer.write(&line.into_bytes())
+                    .expect("[AArch64_extern] Write failed.");
+            },
+            
+            Arm64Type::Global => {
+                let mut line = ".global ".to_string();
+                line.push_str(&ln.name);
+                line.push_str("\n");
+                
+                writer.write(&line.into_bytes())
+                    .expect("[AArch64_global] Write failed.");
+            },
+            
+            Arm64Type::Label => {
+                let mut line = ln.name.clone();
+                line.push_str(":\n");
+                writer.write(&line.into_bytes())
+                    .expect("[AArch64_label] Write failed.");
+            },
+            
+            _ => {},
+        }
+    }
+}
+
+// Builds a function declaration
+fn arm64_build_func(code : &mut Vec<Arm64Instr>, instr : &LtacInstr) {
+    let mut glbl = create_arm64_instr(Arm64Type::Global);
+    glbl.name = instr.name.clone();
+    code.push(glbl);
+
+    let mut instr2 = create_arm64_instr(Arm64Type::Label);
+    instr2.name = instr.name.clone();
+    code.push(instr2);
 }
